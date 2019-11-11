343da96060e8ba6c6ad7b09a08985a314751b281
kinzo25 <kinjal25.chavda@gmail.com>
2019-11-11 13:38:13 -0500

    Login and Registration form changed, event add and edit changed
    Event add and edit don't have an image anymore. Login form will display validation messages upon clicking submit (disable removed). Both login and registration now have proper tabs at the top instead of things that look like buttons. The page header has a logout button now, which needs to be linked to a function. Header will disappear when logged out

--- NGOEventsCKK/src/app/app.component.ts
+++ NGOEventsCKK/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NGOEventsCKK';
+
+  public logged:boolean = true;
+  //the "logged" flag should be true if someone's logged in.
+  //if not, it should be set to false.
+
+  // a logout function
+  logout()
+  {
+    //here's the log out function
+    this.logged=false;
+    //redirect them to home page
+  }
 }
